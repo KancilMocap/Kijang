9c20653fd6ae7fbabab8df06de497c6e1935b571
Paxriel <paxriel@protonmail.com>
2021-09-07 17:23:50 +0800

    Added libfreenect libraries

---
+++ README.md
@@ -0,0 +1,15 @@
+# Kijang
+
+## Build Instructions
+
+1. Create a 'libs' folder in the project root directory.
+2. Find and download the corresponding libfreenect libraries and libfreenect2 libraries from [libfreenect-libs](https://github.com/paxriel/libfreenect-libs).
+3. Extract the files in the two libraries into libs/libfreenect and libs/libfreenect2 respectively.
+
+## Libraries used
+
+### GPL v2 License
+
+- [libfreenect](https://github.com/OpenKinect/libfreenect) v0.6.2
+- [libfreenect2](https://github.com/OpenKinect/libfreenect2) v0.2.1
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDateTime>
+#include <QVersionNumber>
+#include <QWindow>
 
+void onLoadComplete(QWindow *window) {
+    QVersionNumber appVersion(VERSION_MAJOR, VERSION_MINOR, VERSION_MIC);
+    QString versionString = appVersion.toString();
+#ifdef QT_DEBUG
+    QDateTime dateTime = QDateTime::currentDateTime();
+    versionString += "-debug." + QString::number(VERSION_DEBUG) + "+" + dateTime.toString("yyyyMMddhhmmss");
+#endif
+    QString windowTitle = "Kijang (" + versionString + ")";
+    window->setTitle(windowTitle);
+    window->showMaximized();
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +30,9 @@ int main(int argc, char *argv[])
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
+        QWindow *window = qobject_cast<QWindow*>(obj);
+        if (!window) QCoreApplication::exit(-1);
+        onLoadComplete(window);
     }, Qt::QueuedConnection);
     engine.load(url);
 
